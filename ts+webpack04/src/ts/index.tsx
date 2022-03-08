2d4b0ca2cb145240ccab6d2600bc391d73e48281
chenxuewei <chen.xuewei@yilutong.com>
2022-03-08 09:51:18 +0800

    请教问题

--- ts+webpack04/src/ts/index.tsx
+++ ts+webpack04/src/ts/index.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import { render } from 'react-dom';
 import '../css/search.less';
 const logo = require('../images/logo.png');
-try {
-    const rootElement = document.getElementById('root');
-    console.log("运行");
-    const App = () => {
-        return <div className="hello">
-            <div>hello word</div>
-            <img src={logo}></img>
-        </div>
-    };
-    render(<App />, rootElement)
-} catch (e) {
-    console.log('e', e);
-}
+const rootElement = document.getElementById('root');
+console.log("运行");
+
+
+const App = () => {
+	let [show,setShow] = useState<boolean>(false)
+	return (
+		<div className="hello">
+			{show?'nihao':'buhao'}
+			<img src={logo}></img>
+		</div>
+	)
+};
+render(<App />, rootElement)
